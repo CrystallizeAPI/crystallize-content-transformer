607a5e032af2c211fd68661351f025deb61d2cce
Håkon Krogh <hakonhk@gmail.com>
2018-06-25 09:59:03 +0200

    Transpilation for build

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['env', { modules: false }], 'stage-2']
+          }
+        }
+      }
+    ]
   }
 };
